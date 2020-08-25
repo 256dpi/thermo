e72b2cf5fd9934764647142bb64d9089c4baa43f
Joël Gähwiler <joel.gaehwiler@gmail.com>
2020-08-25 23:42:50 +0200

    updated ember

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 import Application from '@ember/application';
+import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 
-import Resolver from './resolver';
 import config from 'thermo/config/environment';
 
 export default class App extends Application {
--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,5 +1,5 @@
-import Application from '../app';
-import config from '../config/environment';
+import Application from 'thermo/app';
+import config from 'thermo/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 
