f1d09b68e7e99b58bb65a69b7320faabde0aff32
Joël Gähwiler <joel.gaehwiler@gmail.com>
2020-09-07 08:24:45 +0200

    use brace expansion

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -11,7 +11,7 @@ export default JSONAPIAdapter.extend(DataAdapterMixin, {
   host: config.blueprint.backend.baseURL,
   namespace: config.blueprint.backend.dataPath,
 
-  headers: computed('session.data.authenticated.access_token', 'session.isAuthenticated', function() {
+  headers: computed('session.{isAuthenticated,data.authenticated.access_token}', function() {
     let headers = {};
     if (this.session.isAuthenticated) {
       headers['Authorization'] = `Bearer ${this.session.data.authenticated.access_token}`;
