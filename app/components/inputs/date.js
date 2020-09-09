d84560b8cf3e4335f80e40c44d6f4b0d0900c704
Joël Gähwiler <joel.gaehwiler@gmail.com>
2020-09-09 09:26:58 +0200

    handle dates as string values for now

--- app/components/inputs/date.js
+++ app/components/inputs/date.js
@@ -1,8 +1,22 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
   label: '',
   value: null,
   hint: null,
-  disabled: false
+  disabled: false,
+
+  stringValue: computed('value', {
+    get() {
+      return this.value.toISOString();
+    },
+    set(_, value) {
+      const date = new Date(Date.parse(value));
+      if (!isNaN(date)) {
+        this.set('value', date);
+      }
+      return value;
+    }
+  })
 });
