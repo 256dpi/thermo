c0c33841fabb33952887170d1f00565b96c2905b
Joël Gähwiler <joel.gaehwiler@gmail.com>
2020-09-10 18:08:19 +0200

    use json excerpt

--- app/helpers/as-json.js
+++
@@ -1,7 +0,0 @@
-import { helper } from '@ember/component/helper';
-
-export function asJson(params) {
-  return JSON.stringify(params[0], null, '  ');
-}
-
-export default helper(asJson);
---
+++ app/helpers/json-excerpt.js
@@ -0,0 +1,11 @@
+import { helper } from '@ember/component/helper';
+
+export function jsonExcerpt([value, length = 32]) {
+  const str = JSON.stringify(value);
+  if (str.length > length) {
+    return str.substr( 0, length) + "...";
+  }
+  return str;
+}
+
+export default helper(jsonExcerpt);
