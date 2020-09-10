67369c8bb43865ee43d3b9eec02fe44b79a7759d
Joël Gähwiler <joel.gaehwiler@gmail.com>
2020-09-10 22:28:24 +0200

    use flatpickr for dates

--- app/components/inputs/date.js
+++ app/components/inputs/date.js
@@ -1,22 +1,9 @@
 import Component from '@ember/component';
-import { computed } from '@ember/object';
 
-export default Component.extend({
-  label: '',
-  value: null,
-  hint: null,
-  disabled: false,
-
-  stringValue: computed('value', {
-    get() {
-      return this.value.toISOString();
-    },
-    set(_, value) {
-      const date = new Date(Date.parse(value));
-      if (!isNaN(date)) {
-        this.set('value', date);
-      }
-      return value;
-    }
-  })
-});
+export default class extends Component {
+  label = '';
+  value = null;
+  hint = null;
+  placeholder = null;
+  disabled = false;
+}
