90d8dd0d70e98beeb0304aaf94104151b98337fe
Joël Gähwiler <joel.gaehwiler@gmail.com>
2020-09-28 15:30:29 +0200

    fix input

--- app/components/inputs/date.js
+++ app/components/inputs/date.js
@@ -1,7 +1,12 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
 
 export default class extends Component {
   get now() {
     return new Date();
+  }
+
+  @action changed(dates) {
+    this.args.changed(dates[0]);
   }
 }
