f87bb7e9e27d9dd9713410aed2b7e66d2c2b3ec2
Joël Gähwiler <joel.gaehwiler@gmail.com>
2021-09-09 21:59:13 +0200

    use page-title addon

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,8 +1,4 @@
 import Route from '@ember/routing/route';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
-export default class extends Route.extend(ApplicationRouteMixin) {
-  activate() {
-    document.title = this.blueprint.title;
-  }
-}
+export default class extends Route.extend(ApplicationRouteMixin) {}
