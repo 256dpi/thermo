b8d929c5e5954ff3ac9e800623a4247315359806
Joël Gähwiler <joel.gaehwiler@gmail.com>
2021-09-10 09:27:49 +0200

    removed session initializer

--- app/initializers/session.js
+++
@@ -1,7 +0,0 @@
-export default {
-  initialize(app) {
-    app.inject('route', 'session', 'service:session');
-    app.inject('controller', 'session', 'service:session');
-    app.inject('component', 'session', 'service:session');
-  },
-};
--- app/routes/table/new.js
+++ app/routes/table/new.js
@@ -1,6 +1,9 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class extends Route {
+  @service session;
+
   beforeModel(transition) {
     // check authentication
     this.session.requireAuthentication(transition, 'sign-in');
