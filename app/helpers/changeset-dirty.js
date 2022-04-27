f1a589dce039523723c13a8d7d0354971994b126
Joël Gähwiler <joel.gaehwiler@gmail.com>
2022-04-27 08:20:21 +0200

    fmt

--- app/helpers/changeset-dirty.js
+++ app/helpers/changeset-dirty.js
@@ -2,8 +2,7 @@ import { helper } from '@ember/component/helper';
 import { get } from '@ember/object';
 
 export function changesetDirty([value, key]) {
-  console.log(value.change, key);
-  return value.change ? get(value.change, key) != undefined : false;
+  return value.change ? get(value.change, key) !== undefined : false;
 }
 
 export default helper(changesetDirty);
