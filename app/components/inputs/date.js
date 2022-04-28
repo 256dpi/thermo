28d6b7de5f6b69a8d9bc61e5c8c21888db42511c
Joël Gähwiler <joel.gaehwiler@gmail.com>
2022-04-28 19:39:14 +0200

    date picker only has a minute precision

--- app/components/inputs/date.js
+++ app/components/inputs/date.js
@@ -6,7 +6,23 @@ export default class extends Component {
     return new Date();
   }
 
+  get value() {
+    if (this.args.value) {
+      const value = new Date(this.args.value.getTime());
+      value.setSeconds(0);
+      value.setMilliseconds(0);
+      return value;
+    } else {
+      return undefined;
+    }
+  }
+
   @action changed(dates) {
+    // skip if not changed
+    if (this.value && dates[0] && this.value - dates[0] === 0) {
+      return;
+    }
+
     this.args.changed(dates[0]);
   }
 }
