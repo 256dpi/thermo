4604fda4112816872144a4722ccb8d0635b4e0d4
Joël Gähwiler <joel.gaehwiler@gmail.com>
2022-04-28 18:50:14 +0200

    updated json excerpt

--- app/helpers/json-excerpt.js
+++ app/helpers/json-excerpt.js
@@ -3,7 +3,7 @@ import { helper } from '@ember/component/helper';
 export function jsonExcerpt([value, length = 32]) {
   const str = JSON.stringify(value) || '';
   if (str.length > length) {
-    return str.substr(0, length) + '...';
+    return str.substring(0, length) + '…';
   }
   return str;
 }
