7b08e5adc0b3bfcde63f713e0ffa5d0890c28f95
Joël Gähwiler <joel.gaehwiler@gmail.com>
2022-05-03 22:50:47 +0200

    properly unwrap value

--- app/components/controls/json.js
+++ app/components/controls/json.js
@@ -2,10 +2,6 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 
 export default class extends Component {
-  get value() {
-    return JSON.stringify(this.args.value, null, '  ');
-  }
-
   @action changed(e) {
     if (this.args.changed) {
       this.args.changed(JSON.parse(e.target.value));
