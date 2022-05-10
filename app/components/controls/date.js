cdd9f812918308d80a8e39e29066435733b9622a
Joël Gähwiler <joel.gaehwiler@gmail.com>
2022-05-10 08:55:58 +0200

    use native input for dates

--- app/components/controls/date.js
+++ app/components/controls/date.js
@@ -1,28 +1,29 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import moment from 'moment';
 
 export default class extends Component {
-  get now() {
-    return new Date();
-  }
-
   get value() {
     if (this.args.value) {
       const value = new Date(this.args.value.getTime());
       value.setSeconds(0);
       value.setMilliseconds(0);
-      return value;
+      return moment(value).local().format(moment.HTML5_FMT.DATETIME_LOCAL);
     } else {
-      return undefined;
+      return '';
     }
   }
 
-  @action changed(dates) {
+  @action changed(e) {
+    // convert local datetime string
+    const value = moment(e.target.value, moment.HTML5_FMT.DATETIME_LOCAL).toDate();
+
     // skip if not changed
-    if (this.value && dates[0] && this.value - dates[0] === 0) {
+    if (this.value && value && this.value - value === 0) {
       return;
     }
 
-    this.args.changed(dates[0]);
+    // yield value
+    this.args.changed(value);
   }
 }
