c02f0f526e550cb51be45e97466f40a801421d66
Joël Gähwiler <joel.gaehwiler@gmail.com>
2024-03-03 18:11:33 +0100

    updated to ember 4.12

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,13 +1,11 @@
 'use strict';
 
 module.exports = {
-  singleQuote: true,
-  printWidth: 120,
   overrides: [
     {
-      files: ['*.hbs'],
+      files: '*.{js,ts}',
       options: {
-        singleQuote: false,
+        singleQuote: true,
       },
     },
   ],
