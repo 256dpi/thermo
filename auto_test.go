f762b2d34628302f5633afd21e300057cf7786b7
Joël Gähwiler <joel.gaehwiler@gmail.com>
2025-10-02 12:25:05 +0200

    test auto function

--- auto_test.go
+++ auto_test.go
@@ -2,9 +2,39 @@ package thermo
 
 import (
 	"testing"
+	"time"
 
+	"github.com/256dpi/fire/blaze"
+	"github.com/256dpi/fire/coal"
+	"github.com/256dpi/fire/stick"
 	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/bson"
 )
+
+type toggle struct {
+	State bool `json:"state"`
+}
+
+type subItem struct {
+	Name    string   `json:"name"`
+	Scale   float64  `json:"scale"`
+	Toggles []toggle `json:"toggles"`
+	stick.NoValidation `json:"-" bson:"-"`
+}
+
+type item struct {
+	coal.Base `json:"-" bson:",inline" coal:"items"`
+	Name      string      `json:"name"`
+	State     bool        `json:"state"`
+	Count     int         `json:"count"`
+	Raw       bson.M      `json:"raw"`
+	SubItems  []subItem   `json:"sub-items"`
+	File      *blaze.Link `json:"file"`
+	Color     string      `json:"color"`
+	Created   time.Time   `json:"created" coal:"fire-created-timestamp"`
+	Updated   time.Time   `json:"updated" coal:"fire-updated-timestamp"`
+	stick.NoValidation `json:"-" bson:"-"`
+}
 
 func TestTitle(t *testing.T) {
 	assert.Equal(t, "Foo Bar", Title("FooBar"))
@@ -13,4 +43,159 @@ func TestTitle(t *testing.T) {
 	assert.Equal(t, "PDF", Title("PDF"))
 	assert.Equal(t, "MD5", Title("MD5"))
 	assert.Equal(t, "Some 3", Title("Some3"))
+}
+
+func TestAuto(t *testing.T) {
+	model := Auto(&item{}, "items", "Item", "Items", nil)
+	assert.Equal(
+		t,
+		Model{
+			Name:      "items",
+			Singular:  "Item",
+			Plural:    "Items",
+			Creatable: true,
+			Editable:  true,
+			Deletable: true,
+			Attributes: []Attribute{
+				{
+					Name:    "name",
+					Kind:    "value",
+					Type:    "string",
+					Default: "",
+				},
+				{
+					Name:    "state",
+					Kind:    "value",
+					Type:    "boolean",
+					Default: false,
+				},
+				{
+					Name:    "count",
+					Kind:    "value",
+					Type:    "number",
+					Default: 0,
+				},
+				{
+					Name: "raw",
+					Kind: "value",
+				},
+				{
+					Name: "sub-items",
+					Kind: "value",
+				},
+				{
+					Name: "file",
+					Kind: "value",
+					Type: "file",
+				},
+				{
+					Name:    "color",
+					Kind:    "value",
+					Type:    "string",
+					Default: "",
+				},
+				{
+					Name: "created",
+					Kind: "value",
+					Type: "date",
+				},
+				{
+					Name: "updated",
+					Kind: "value",
+					Type: "date",
+				},
+			},
+			Columns: []Column{
+				{
+					Title:  "Name",
+					Key:    "name",
+					Format: "literal",
+				},
+				{
+					Title:  "State",
+					Key:    "state",
+					Format: "literal",
+				},
+				{
+					Title:  "Count",
+					Key:    "count",
+					Format: "literal",
+				},
+				{
+					Title: "Raw",
+					Key:   "raw",
+				},
+				{
+					Title: "Sub Items",
+					Key:   "sub-items",
+				},
+				{
+					Title:  "File",
+					Key:    "file",
+					Format: "file",
+				},
+				{
+					Title:  "Color",
+					Key:    "color",
+					Format: "literal",
+				},
+				{
+					Title:  "Created",
+					Key:    "created",
+					Format: "absolute-date",
+				},
+				{
+					Title:  "Updated",
+					Key:    "updated",
+					Format: "absolute-date",
+				},
+			},
+			Fields: []Field{
+				{
+					Label:   "Name",
+					Key:     "name",
+					Control: "string",
+				},
+				{
+					Label:   "State",
+					Key:     "state",
+					Control: "boolean",
+				},
+				{
+					Label:   "Count",
+					Key:     "count",
+					Control: "number",
+				},
+				{
+					Label: "Raw",
+					Key:   "raw",
+				},
+				{
+					Label: "Sub Items",
+					Key:   "sub-items",
+				},
+				{
+					Label:   "File",
+					Key:     "file",
+					Control: "file",
+				},
+				{
+					Label:   "Color",
+					Key:     "color",
+					Control: "string",
+				},
+				{
+					Label:   "Created",
+					Key:     "created",
+					Control: "date",
+				},
+				{
+					Label:   "Updated",
+					Key:     "updated",
+					Control: "date",
+				},
+			},
+		},
+		model,
+	)
 }
